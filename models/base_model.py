194fb037d9ca8ab9704b518e52e377d0cff623b2
Ifeanyichukwu Favour <ifeanyifavour74@gmail.com>
2023-05-14 18:28:13 -0700

    Fixed bug in base model. Test runs perfectly

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
                     setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)     
+                    setattr(self, key, value)     
         else:
              self.id = str(uuid.uuid4())
              self.created_at = datetime.now()
